b7038cea3939161cb0bf4d6cfef6ffdad485f79d
HoanBao7904 <nguyenhoanbao4@gmail.com>
2026-08-27 23:35:48 +0700

    test chat emit giua hai user bang socket.io

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,9 @@ import likesRouter from './routes/likes.route'
 import useRoutes from './routes/users.routes'
 import '~/utils/s3'
 import cors from 'cors'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import { da } from '@faker-js/faker/.'
 // import '~/utils/fake'
 config()
 // console.log(path.resolve('src/templates/verify-email.html'))
@@ -22,7 +25,11 @@ const PORT = process.env.PORT || 4000
 
 // console.log(process.argv)
 const app = express()
+
+const httpServer = createServer(app)
+
 app.use(cors())
+
 app.use(express.json())
 // đây là middleware, nó sẽ chạy trước khi vào route, nó sẽ parse body của request thành json
 // nếu không có cái này thì req.body sẽ là undefined
@@ -47,12 +54,57 @@ app.use('/static', staticRouter)
 
 app.use(defaultErrorHandler)
 
-app.listen(PORT, () => {
-  // nghiax la khi
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000'
+  }
+})
+
+const users: {
+  [key: string]: {
+    socket_id: string
+  }
+} = {}
+
+io.on('connection', (socket) => {
+  console.log(`userid: ${socket.id} connected `)
+  console.log(socket.handshake.auth)
+  console.log(socket.handshake.auth._id1)
+
+  const userId = socket.handshake.auth._id1
+
+  users[userId] = {
+    socket_id: socket.id
+  }
+  console.log(users)
+
+  socket.on('private message', (data) => {
+    //lấy id socket từ userId muốn gửi đến(xác định được muốn gửi tới thz đó)
+    const receiver_socket_id = users[data.to]?.socket_id
+    if (!receiver_socket_id) {
+      return
+    }
+
+    console.log('receiver_socket_id', receiver_socket_id)
+    socket.to(receiver_socket_id).emit('receiver private message', {
+      content: data.value,
+      from: userId
+    })
+  })
+  socket.on('disconnect', () => {
+    delete users[userId]
+    console.log(`userid: ${socket.id} disconnected `)
+  })
+})
+
+httpServer.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 })
-//
-// console.log(pick({ a: '1', b: 3 }, ['c']))
+
+// app.listen(PORT, () => {
+//   // nghiax la khi
+//   console.log(`listening on port ${PORT}`)
+// })
 
 //hoanbao79
 //2FvCL34jtuN9Amku
